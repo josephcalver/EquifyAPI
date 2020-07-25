841253bf6edc934e4b3cbf3b1186320029d18a99
Joseph Calver <josephcalver@gmail.com>
2020-07-25 11:45:15 +0100

    Create and register DbContext; add database dependencies

---
+++ EquifyAPI/Data/EquifyContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EquifyAPI.Models
+{
+    public class EquifyContext : DbContext
+    {
+        public EquifyContext(DbContextOptions<EquifyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Deal> Deal { get; set; }
+    }
+}
